c6ac026456f7eaf4dfafa54adac7595566d8e34e
Vivek W <92390419+AryanVBW@users.noreply.github.com>
2024-08-23 21:44:14 +0530

    Update MacOs.sh

--- MacOs.sh
+++ MacOs.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 security find-generic-password -ga "Wi-Fi" | grep "password:" | cut -d '"' -f 2
+
+
+
+
+
+
+#!/bin/bash
+
+security find-generic-password -ga "Wi-Fi" | while read line; do
+    ssid=$(echo $line | grep "label:" | cut -d '"' -f 2)
+    password=$(echo $line | grep "password:" | cut -d '"' -f 2)
+    if [ -n "$ssid" ] && [ -n "$password" ]; then
+        echo "Network Name (SSID): $ssid"
+        echo "Password: $password"
+        echo "-------------------------"
+    fi
+done
